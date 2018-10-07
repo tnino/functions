0ffeaeef0d1559f5e3a15506ba42152d063772fc
tnino <30389051+tnino@users.noreply.github.com>
2018-10-07 11:05:58 -0500

    Update functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,14 +1,19 @@
-// Returns whatever value is passed as the argument.
-//
-// EX: identity(6) --> 6
+<script>
+Returns whatever value is passed as the argument.
+EX: identity(6) --> 6
 
-print(abs(6))
+let identity = function(num){
+  return num;
+}
+console.log(identity(6));
+
 
 // Return an array of the first n elements of an array. If n is undefined,
 // return just the first element.
 //
 // EX: first([1, 2, 3, 4, 5, 6], 2) --> [1, 2]
 // EX: first([10, 34, 68, 19]) --> 10
+
 let first =  function(array, n) {
       if (array == null) 
       return void 0;
@@ -19,9 +24,8 @@ let first =  function(array, n) {
     return array.slice(0, n);
   };
 
-console.log(first([1,2,3,4,5,6]));
-console.log(first([10,34,68,19])
-)
+console.log(first([1,2,3,4,5,6],4));
+console.log(first([10,34,68,19]));
 
 https://www.w3resource.com/w3r_images/javascript-array-exercise-3.png
 
@@ -30,8 +34,22 @@ https://www.w3resource.com/w3r_images/javascript-array-exercise-3.png
 //
 // EX: last([1, 2, 3, 4, 5], 2) --> [4, 5]
 // EX: last([10, 34, 68, 19]) --> 19
-let last = (array, n) => {
+let last =  function(array, n) {
+  let id = array.length-1;
+  if(n !== null){
+    let elementos = array.length - n;
+    
+  for(let i = 0; i<elementos; i++){
+array.splice(0,1);
+  }
+  return array;
+  }else{
+    return array[id];
+  }
 };
+
+                  
+console.log(last([1,2,3,4,5,6],4));
 
 
 
@@ -41,7 +59,8 @@ let last = (array, n) => {
 //
 // each() does not have a return value, but rather simply runs the
 // iterator function over each item in the input collection.
- let each = function(collection, iterator){
+ 
+  let each = function(collection, iterator){
   if(Array.isArray(collection)){
     for(var i = 0; i < collection.length; i++){
     iterator(collection[i], i, collection);
@@ -52,6 +71,19 @@ let last = (array, n) => {
     }
   }
 };
+
+each({"nombre":"asd"},function(a,b,c){console.log(a,b,c)})
+
+/*
+let array = [123,243,6123,6,23,62,3,43]
+array[0];
+
+let objeto = {
+  "nombre":"sdf",
+  "apellido":"asfd"
+}
+objeto.nombre
+*/
 
 // Returns the index at which value can be found in the array, or -1 if value
 // is not present in the array.
@@ -88,7 +120,7 @@ let uniq_numbers = [... new Set(numbers)];
 console.log(uniq_numbers);
 
 [ 1, 2, 3, 4, 5 ]
-=> undefined
+
 
 
 
@@ -99,8 +131,17 @@ console.log(uniq_numbers);
 // map({firstName: 'Kayla', lastName: 'Handy', age: 29}, (item) => {
 //    return item[key]
 // }) --> ['Kayla', 'Handy', 29]
-let map = (collection, iterator) => {
-};
+
+ let myObject = {firstName: 'Kayla', lastName: 'Handy', age: 29};
+
+Object.keys(myObject).map((key, index) => myObject[key]);
+
+console.log(myObject);
+
+
+
+
+https://developer.mozilla.org/en-US/docs/Archive/Web/Iterator
 
 // Reduces an array or object to a single value by repetitively calling
 // iterator(accumulator, item) for each item. accumulator should be
@@ -132,8 +173,18 @@ let contains = (collection, target) => {
 // EX: every([1, 2, 3], (item) => {
 //    item < 5
 // }) --> true
+
 let every = (collection, iterator) => {
+  let all = true;
+  for(let i = 0; i<collection.length; i++){
+    if(!iterator(collection[i])){
+      all = false;
+    }
+  }
+    console.log(all);
 };
+
+every([1,2,3,4,5],(item)=> item<5)
 
 // Determine whether any of the elements pass a truth test.
 //
@@ -142,3 +193,4 @@ let every = (collection, iterator) => {
 // }) --> true
 let some = (collection, iterator) => {
 };
+</script>
